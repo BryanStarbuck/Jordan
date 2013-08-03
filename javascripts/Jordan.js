2cf9d364ed379e0cdd555632e05ccf9d7042c978
Bryan Starbuck <Bryan@TheStarbucks.com>
2013-08-03 07:19:22 -0700

    Update Jordan.js

--- javascripts/Jordan.js
+++ javascripts/Jordan.js
@@ -1,1 +1,10 @@
- 
+var dad = new Movable('Dad', 3, 5); 
+var mom = new Movable('Mom', 5, 59); 
+var jordan = new Movable('Jordan', 40, 20); 
+var house = new Movable('House', 60, 5); 
+var candyhouse = new Movable('CandyHouse', 60, 75); 
+
+
+
+
+
