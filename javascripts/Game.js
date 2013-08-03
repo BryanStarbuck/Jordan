5c8dc994ba2ef193e3c8797f9d687f67945f9d5b
Bryan Starbuck <Bryan@TheStarbucks.com>
2013-08-03 07:12:40 -0700

    Update Game.js

--- javascripts/Game.js
+++ javascripts/Game.js
@@ -1,17 +1,13 @@
-
 function Movable(id, x, y) {
   this.currentX = x;
   this.currentY = y;
   this.id = id;
 }
 
-
 Movable.prototype.sayHello = function()
 {
   alert ('hello');
 };
-
-
 
 Movable.prototype.move = function(x, y)
 {
@@ -19,8 +15,22 @@ Movable.prototype.move = function(x, y)
   destY = destY + y;
 };
 
-
 Movable.prototype.MoveUp = function(x, y)
 {
   move.move(this.destX, this.destY - 10);
+};
+
+Movable.prototype.MoveDown = function(x, y)
+{
+  move.move(this.destX, this.destY + 10);
+};
+
+Movable.prototype.MoveLeft = function(x, y)
+{
+  move.move(this.destX - 10, this.destY);
+};
+
+Movable.prototype.MoveRight = function(x, y)
+{
+  move.move(this.destX + 10, this.destY);
 };
